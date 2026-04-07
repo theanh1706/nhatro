7430c31d160d1c03449fb8b3d8880282da9e9a8c
theanh1706 <50204439+theanh1706@users.noreply.github.com>
2026-04-07 21:43:44 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,4 @@
-const CACHE_NAME = "tro-app-v1";
-
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", e => {
+  self.skipWaiting();
 });
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener("fetch", e => {});
